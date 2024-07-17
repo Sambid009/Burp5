55fdbbd8aa755b8a1a6d766f12ad980da9b3cf36
Sambid009 <sambidahal9@gmail.com>
2024-07-17 07:05:30 +0545

    new activity ListViewExampleActivity created and ListView is done

---
+++ app/src/main/java/com/earth/burp5/ListViewExampleActivity.java
@@ -0,0 +1,44 @@
+package com.earth.burp5;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class ListViewExampleActivity extends AppCompatActivity {
+
+    ListView listView;
+    String[] languages = {"Java","C","C++","Python","JavaScript","php","Ruby","Perl","Scala","Kotlin"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_list_view_example);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        listView = findViewById(R.id.listView);
+        ArrayAdapter arrayAdapter = new ArrayAdapter(ListViewExampleActivity.this, android.R.layout.simple_list_item_1, languages);
+        listView.setAdapter(arrayAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(ListViewExampleActivity.this, "Selected:: " + languages[position], Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+
+    }
+}
